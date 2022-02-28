1c0c5d7a4939f901ca8bcbbb639104d4eca46f0a
Ricardo Mestre <ricardojoao85@gmail.com>
2022-02-28 22:51:11 +0000

    add sample to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-const App = () => (<div></div>)
+import {useEffect, useState} from 'react';
+import { getNRandomItems } from './utils/random';
+import { ISymbol, symbols } from './db/symbols';
+import Sample from './components/Sample';
+const App = () => {
+    const [data] = useState(getNRandomItems(symbols, 10));
+    const [found, setFound] = useState<Array<ISymbol>>([]);
+
+    useEffect(() => setFound(prev => [...prev, data[0]]),[]);
+
+    return (
+    <main>
+        <Sample sample={data} itemsFound={found} />
+    </main>
+    )
+};
 
 export default App;
